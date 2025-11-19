e6d13150b2a0bd6a45ffcba2b427970c30093d07
kenan <k.karimli@ufaz.az>
2025-11-19 14:29:25 +0400

    login useranme etc

--- UI.py
+++ UI.py
@@ -24,23 +24,21 @@ class LoginPage(Frame):
         Frame.__init__(self, parent, bg='light blue')
         self.controller = controller
 
+        entered_username = tk.Label(self, text="Username:")
+        entered_username.grid(row=2, column=0, pady=10, padx=20)
+        self.entered_username = tk.Entry(self, show="*")
+        self.entered_username.grid(row=2, column=1, pady=10)
 
         card_label = tk.Label(self, text="Card Number:")
         card_label.grid(row=0, column=0, pady=(120, 10), padx=20)
-        self.card_entry = tk.Entry(self)
-        self.card_entry.grid(row=0, column=1, pady=(120, 10))
+        self.card_number = tk.Entry(self)
+        self.card_number.grid(row=0, column=1, pady=(120, 10))
 
 
-        pin_label = tk.Label(self, text="PIN:")
-        pin_label.grid(row=1, column=0, pady=10, padx=20)
-        self.pin_entry = tk.Entry(self, show="*")
-        self.pin_entry.grid(row=1, column=1, pady=10)
-
-
-        cvc_label = tk.Label(self, text="CVC:")
-        cvc_label.grid(row=2, column=0, pady=10, padx=20)
-        self.cvc_entry = tk.Entry(self, show="*")
-        self.cvc_entry.grid(row=2, column=1, pady=10)
+        entered_pin = tk.Label(self, text="PIN:")
+        entered_pin.grid(row=1, column=0, pady=10, padx=20)
+        self.entered_pin = tk.Entry(self, show="*")
+        self.entered_pin.grid(row=1, column=1, pady=10)
 
         # --- Login Button ---
         login_button = tk.Button(self, text="Login", command=self.login_user, width=12, height=1)
