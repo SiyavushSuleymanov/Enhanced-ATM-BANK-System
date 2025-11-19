b984d86ae272389e86583357918788d7c260dd80
kenan <k.karimli@ufaz.az>
2025-11-19 14:31:13 +0400

    updated

--- UI.py
+++ UI.py
@@ -25,20 +25,14 @@ class LoginPage(Frame):
         self.controller = controller
 
         entered_username = tk.Label(self, text="Username:")
-        entered_username.grid(row=2, column=0, pady=10, padx=20)
+        entered_username.grid(row=1, column=0, pady=10, padx=20)
         self.entered_username = tk.Entry(self, show="*")
-        self.entered_username.grid(row=2, column=1, pady=10)
-
-        card_label = tk.Label(self, text="Card Number:")
-        card_label.grid(row=0, column=0, pady=(120, 10), padx=20)
-        self.card_number = tk.Entry(self)
-        self.card_number.grid(row=0, column=1, pady=(120, 10))
-
+        self.entered_username.grid(row=1, column=1, pady=10)
 
         entered_pin = tk.Label(self, text="PIN:")
-        entered_pin.grid(row=1, column=0, pady=10, padx=20)
+        entered_pin.grid(row=2, column=0, pady=10, padx=20)
         self.entered_pin = tk.Entry(self, show="*")
-        self.entered_pin.grid(row=1, column=1, pady=10)
+        self.entered_pin.grid(row=2, column=1, pady=10)
 
         # --- Login Button ---
         login_button = tk.Button(self, text="Login", command=self.login_user, width=12, height=1)
