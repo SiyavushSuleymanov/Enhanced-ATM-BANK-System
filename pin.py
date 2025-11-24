91fa6b5c44f22b8d75ac397efd40de9624338b77
DeRasser <derasser08@gmail.com>
2025-11-25 01:22:11 +0400

    pin upgraded

--- pin.py
+++ pin.py
@@ -4,19 +4,22 @@ pin_len = 4
 
 def pressed(event):
     global pin
-    if event.keysym.isdigit():
+    if event.keysym.isdigit() and len(str(pin)) != 4:
         pin += event.keysym
         upgrade()
         if upgrade() == 1:
-            pin = int(pin[:4]) #OUR ENTERED 4-digit PIN (result)
-            print(pin)
+            pin = pin[:4] #OUR ENTERED 4-digit PIN (result)
+            print(int(pin))
     elif event.keysym == "BackSpace":
-        delete()
-        pin = pin[:-1]
+        if type(pin) == str:
+            delete()
+            pin = pin[:-1]
+        elif type(pin) == int:
+            pass
 
 def upgrade():
     if len(pin) <= pin_len:
-        dots[len(pin)-1].config(text='◉', font=("Arial", 30))
+        dots[len(pin)-1].config(text='◉', font=("Arial", 50))
     if len(pin) == pin_len:
         return 1
 
