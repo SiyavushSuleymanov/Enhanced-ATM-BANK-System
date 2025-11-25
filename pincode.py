e9fd844da6b329ad91b230711b2d72faaa3d99fe
DeRasser <derasser08@gmail.com>
2025-11-25 07:56:37 +0400

    Comments and frames added. The names of variables are changed to understandable.

--- pincode.py
+++ pincode.py
@@ -1,47 +1,45 @@
 from tkinter import *
 
-pin_len = 4
+pin_len = 4 #The length of the PIN
 
-def pressed(digit):
+def pressed(ent_digit):
     global pin
-    if digit.keysym.isdigit() and len(str(pin)) != 4:
-        pin += digit.keysym
+    if ent_digit.keysym.isdigit() and len(str(pin)) != 4:
+        pin += ent_digit.keysym
         upgrade()
         if upgrade() == 1:
             pin = pin[:4] #OUR ENTERED 4-digit PIN - (result)
             print(int(pin))
-    elif digit.keysym == "BackSpace":
+    elif ent_digit.keysym == "BackSpace":
         if type(pin) == str:
             delete()
             pin = pin[:-1]
         elif type(pin) == int:
             pass
 
-def upgrade():
+def upgrade(): #Upgrades dot list
     if len(pin) <= pin_len:
-        dot_list[len(pin)-1].config(text='◉', font=("Arial", 50))
+        fdot_list[len(pin)-1].config(text='◉', font=("Arial", 50))
     if len(pin) == pin_len:
         return 1
 
-def delete():
+def delete(): #Deletes dot list
     if len(pin) <= pin_len:
-        dot_list[len(pin)-1].config(text='〇', font=("Arial", 30))
+        fdot_list[len(pin)-1].config(text='〇', font=("Arial", 30))
 
 
 window = Tk()
 window.title("Enter the PIN")
-
-pin = ""
-
-frm = Frame(window)
+frm = Frame(window) #Frame to make dots be together
 frm.pack()
+pin = "" #Current (entered) PIN
 
-dot_list = []
+fdot_list = [] #List where dots will be added
 for _ in range(pin_len):
     dot = Label(frm, text='〇', font=('Arial', 30)) #Dots
     dot.pack(side="left")
-    dot_list.append(dot)
+    fdot_list.append(dot)
 
-digit = window.bind('<Key>', pressed) #Entered digit
+ent_digit = window.bind('<Key>', pressed) #Entered digit
 
 window.mainloop()
